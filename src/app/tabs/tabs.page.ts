22117a6f11d3fb2a5d612134daf6df459cfb6501
Matthias Eckstaedt <matthias.eckstaedt@sap.com>
2020-08-11 14:19:21 +0200

    add duration slider

--- src/app/tabs/tabs.page.ts
+++ src/app/tabs/tabs.page.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { AudioService } from '../services/audio.service';
+import { IonRange } from '@ionic/angular';
 
 @Component({
   selector: 'app-tabs',
@@ -8,20 +9,67 @@ import { AudioService } from '../services/audio.service';
 })
 export class TabsPage {
 
+  @ViewChild('range', { static: false }) range: IonRange;
+
   playing = false;
   title = '';
+  progress = 0;
+  focus = false;
+  curTime = '00:00';
+  duration = '00:00';
 
   constructor(
     private audioService: AudioService
   ) {
     this.audioService.onChange().subscribe((res: any) => {
       this.playing = res.playing;
+      if (this.playing) {
+        this.updateProgress();
+      }
       this.title = res.title;
     });
   }
 
+  seek() {
+    const newValue: number = +this.range.value;
+    this.audioService.setSeek(this.audioService.getDuration() * (newValue / 100));
+    this.focus = false;
+    this.updateProgress();
+  }
+
   play() {
     this.audioService.play();
+  }
+
+  updateProgress() {
+    if (!this.focus) {
+      if (!isNaN(this.audioService.getSeek())) {
+        this.progress = (this.audioService.getSeek() / this.audioService.getDuration()) * 100 || 0;
+        this.curTime = this.getMinString(Math.round(this.audioService.getSeek()));
+        this.duration = this.getMinString(Math.round(this.audioService.getDuration()));
+      }
+      setTimeout(() => {
+        this.updateProgress();
+      }, 500);
+    }
+  }
+
+  getMinString(time: number): string {
+    const hours: number | string = Math.floor(time / 3600);
+    let minutes: number | string = Math.floor((time - (hours * 3600)) / 60);
+    let seconds: number | string = time - (hours * 3600) - (minutes * 60);
+
+    if (minutes < 10) {
+      minutes = '0' + minutes;
+    }
+    if (seconds < 10) {
+      seconds = '0' + seconds;
+    }
+    return minutes + ':' + seconds;
+  }
+
+  onRangeFocus() {
+    this.focus = true;
   }
 
 }
