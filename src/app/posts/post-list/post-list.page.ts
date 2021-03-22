73941241f4ee9269118ad9aaeff98ef8900014e3
Matthias Eckstaedt <matthias.eckstaedt@sap.com>
2021-03-22 14:26:40 +0100

    add fixes to skeleton

--- src/app/posts/post-list/post-list.page.ts
+++ src/app/posts/post-list/post-list.page.ts
@@ -29,7 +29,7 @@ export class PostListPage implements OnInit {
   rubriken = [];
   page = 1;
   count = null;
-  items = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+  items = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16];
   searchTerm = '';
   currentRubrik: string = 'all';
   ausgaben: Category[] = [];
