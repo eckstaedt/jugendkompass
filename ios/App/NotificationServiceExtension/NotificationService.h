9dec0580f99b971e8fd4fe45c2a0f5df32597e67
Matthias Eckstaedt <matthias.eckstaedt@sap.com>
2021-04-14 10:18:31 +0200

    fix: cannot add image to push notification

---
+++ ios/App/NotificationServiceExtension/NotificationService.h
@@ -0,0 +1,12 @@
+//
+//  NotificationService.h
+//  NotificationServiceExtension
+//
+//  Created by Eckstaedt, Matthias on 14.04.21.
+//
+
+#import <UserNotifications/UserNotifications.h>
+
+@interface NotificationService : UNNotificationServiceExtension
+
+@end
