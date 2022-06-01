0e790bf4b9fb3c83cad60795b10b69f03b30a2f6
Matthias Eckstaedt <matthias.eckstaedt@sap.com>
2022-06-01 08:52:49 +0200

    clean cloud functions

--- cloud-functions/functions/src/index.ts
+++ cloud-functions/functions/src/index.ts
@@ -1,228 +1,20 @@
 //import libraries
 import * as functions from 'firebase-functions';
 import * as admin from 'firebase-admin';
-import * as express from 'express';
-import * as uuid from 'uuid';
-import * as cors from 'cors';
 import * as request from "request-promise-native";
 import * as dayjs from 'dayjs';
+import * as telegraf from 'telegraf';
+import { createClient } from '@supabase/supabase-js';
 
-const options: cors.CorsOptions = {
-  allowedHeaders: [
-    'Origin',
-    'X-Requested-With',
-    'Content-Type',
-    'Accept',
-    'X-Access-Token',
-    'password',
-  ],
-  origin: '*',
-  methods: 'GET,HEAD,OPTIONS,PUT,PATCH,POST,DELETE',
-  preflightContinue: false,
-};
 const url: string = 'https://wp.jugendkompass.com/wp-json/wp/v2/';
 const impulsesId: number = 9;
 const vdtId: number = 42;
 // const url: string = 'https://eckstaedt-webdesign.com/wp-json/wp/v2/';
 // const impulsesId: number = 19;
 
-//initialize firebase inorder to access its services
-admin.initializeApp(functions.config().firebase);
+admin.initializeApp();
 
-//initialize express server
-const app = express();
-const main = express();
-
-main.use(cors(options));
-//add the path to receive request and set json as bodyParser to process the body
-main.use('/', app);
-
-//initialize the database and the collection
 const db = admin.firestore();
-const oneTimeKeyCollection = 'oneTimeKeyCollection';
-
-//define google cloud function name
-export const oneTimeKeys = functions.https.onRequest(main);
-
-function isAuthenticated(password: string) {
-  return password === 'JukoDev2021' ? true : false;
-}
-
-//  verify Client-Key based on the db-entries
-app.get('/verifyOneTimeKey/:keyValue', async (req: any, res: any) => {
-  try {
-    const paramClientKey = req.params.keyValue;
-    const keyQuerySnapshot = await db.collection(oneTimeKeyCollection).get();
-    const oneTimeKeys: any[] = [];
-    keyQuerySnapshot.forEach((doc: { id: string, data: Function }) => {
-      oneTimeKeys.push({
-        id: doc.id,
-        data: doc.data(),
-      });
-    });
-
-    const clientKey = oneTimeKeys.find(
-      key => key.data['value'] == paramClientKey,
-    );
-
-    if (clientKey) {
-      if (clientKey.data.remainingKeyCount < 1) {
-        res.status(403).send('Error: Invalid One-Time-Key!');
-      } else {
-        clientKey.data.remainingKeyCount -= 1;
-        await db.collection(oneTimeKeyCollection).doc(clientKey.id).update({
-          remainingKeyCount: clientKey.data.remainingKeyCount,
-          usedKeys: admin.firestore.FieldValue.increment(1)
-        });
-        res.status(200).json(clientKey);
-      }
-    } else {
-      res.status(403).send('Error: Invalid One-Time-Key!');
-    }
-  } catch (error) {
-    res.status(500).send(error);
-  }
-});
-
-// Create new key
-app.post('/oneTimeKeys', async (req: any, res: any) => {
-  if (!isAuthenticated(req.header('password'))) {
-    res.status(403).send('Invalid Request! Access Not Allowed!');
-    return;
-  }
-
-  try {
-    const oneTimeKey: any = {
-      value: req.body['value'] ? req.body['value'] : uuid.v4().substr(0, 8),
-      remainingKeyCount: req.body['countKeys'],
-    };
-
-    const keyCollectionSnapshot = await db
-      .collection(oneTimeKeyCollection)
-      .get();
-    const oneTimeKeys: any[] = [];
-    keyCollectionSnapshot.forEach((doc: { id: string, data: Function }) => {
-      oneTimeKeys.push({
-        id: doc.id,
-        data: doc.data(),
-      });
-    });
-    const clientKey = oneTimeKeys.find(
-      key => key.data.value == oneTimeKey.value,
-    );
-
-    if (clientKey) {
-      await db
-        .collection(oneTimeKeyCollection)
-        .doc(clientKey.id)
-        .update(oneTimeKey);
-    } else {
-      await db.collection(oneTimeKeyCollection).add(oneTimeKey);
-    }
-
-    res.status(201).json(oneTimeKey);
-  } catch (error) {
-    res
-      .status(400)
-      .send(`Invalid Post-Request! Expected: JSON-Object countKeys: number`);
-  }
-});
-
-//get all keys
-app.get('/oneTimeKeys', async (req: any, res: any) => {
-  if (!isAuthenticated(req.header('password'))) {
-    res.status(403).send('Invalid Request! Access Not Allowed!');
-    return;
-  }
-
-  try {
-    const keyCollectionSnapshot = await db
-      .collection(oneTimeKeyCollection)
-      .get();
-    const oneTimeKeys: any[] = [];
-    keyCollectionSnapshot.forEach((doc: { id: string, data: Function }) => {
-      oneTimeKeys.push({
-        id: doc.id,
-        data: doc.data(),
-      });
-    });
-    res.status(200).json(oneTimeKeys);
-  } catch (error) {
-    res.status(500).send(error);
-  }
-});
-
-//get a single key
-app.get('/oneTimeKeys/:keyId', async (req: any, res: any) => {
-  if (!isAuthenticated(req.header('password'))) {
-    res.status(403).send('Invalid Request! Access Not Allowed!');
-    return;
-  }
-
-  const keyCollectionSnapshot = await db.collection(oneTimeKeyCollection).get();
-  const oneTimeKeys: any[] = [];
-  keyCollectionSnapshot.forEach((doc: { id: string, data: Function }) => {
-    oneTimeKeys.push({
-      id: doc.id,
-      data: doc.data(),
-    });
-  });
-  const clientKey = oneTimeKeys.find(key => key.data.value == req.params.keyId);
-
-  db.collection(oneTimeKeyCollection)
-    .doc(clientKey.id)
-    .get()
-    .then((key: any) => {
-      if (!key.exists) throw new Error('Key not found');
-      res.status(200).json({ id: key.id, data: key.data() });
-    })
-    .catch((error: Error) => res.status(500).send(error));
-});
-
-// Delete a key
-app.delete('/oneTimeKeys/:keyId', async (req: any, res: any) => {
-  if (!isAuthenticated(req.header('password'))) {
-    res.status(403).send('Invalid Request! Access Not Allowed!');
-    return;
-  }
-
-  const keyCollectionSnapshot = await db.collection(oneTimeKeyCollection).get();
-  const oneTimeKeys: any[] = [];
-  keyCollectionSnapshot.forEach((doc: { id: string, data: Function }) => {
-    oneTimeKeys.push({
-      id: doc.id,
-      data: doc.data(),
-    });
-  });
-  const clientKey = oneTimeKeys.find(key => key.data.value == req.params.keyId);
-
-  db.collection(oneTimeKeyCollection)
-    .doc(clientKey.id)
-    .delete()
-    .then(() => res.status(204).send())
-    .catch(function (error: Error) {
-      res.status(404).send(error);
-    });
-});
-
-// Delete all Keys
-app.delete('/oneTimeKeys', async (req: any, res: any) => {
-  if (!isAuthenticated(req.header('password'))) {
-    res.status(403).send('Invalid Request! Access Not Allowed!');
-    return;
-  }
-  const keyCollectionSnapshot = await db.collection(oneTimeKeyCollection).get();
-
-  keyCollectionSnapshot.forEach((key: { id: string }) => {
-    db.collection(oneTimeKeyCollection)
-      .doc(key.id)
-      .delete()
-      .then(() => res.status(204).send('Collection successfully deleted!'))
-      .catch((error: Error) => {
-        res.status(500).send(error);
-      });
-  });
-});
 
 exports.sendTestPush = functions.https.onCall((data: any, _: functions.https.CallableContext) => {
   const payload: admin.messaging.MessagingPayload = {
@@ -275,7 +67,7 @@ exports.sendVdt = functions.pubsub.schedule('0 8 * * *').timeZone("Europe/Berlin
       },
       // data: data.data
     };
-    
+
     admin.messaging().sendToTopic('vdt', payload);
 
     success = true;
@@ -312,7 +104,7 @@ exports.sendImpulse = functions.pubsub.schedule('0 8 * * *').timeZone("Europe/Be
         impulse: data.id.toString()
       }
     };
-    
+
     admin.messaging().sendToTopic('impulse', payload);
 
     success = true;
@@ -330,20 +122,20 @@ exports.syncPostsWithWordPress = functions.pubsub.schedule('0 * * * *').timeZone
   let page: number = 1;
 
   const getPosts: Function = async () => {
-      const options: any = {
-          uri: `${url}posts?_embed&per_page=100&page=${page.toString()}`,
-      };
-      const res: any = await request.get(options);
-      const newPosts: any[] = JSON.parse(res);
-      posts = posts.concat(newPosts);
-      if (newPosts.length === 100) {
-          page += 1
-          await getPosts(page);
-      }
+    const options: any = {
+      uri: `${url}posts?_embed&per_page=100&page=${page.toString()}`,
+    };
+    const res: any = await request.get(options);
+    const newPosts: any[] = JSON.parse(res);
+    posts = posts.concat(newPosts);
+    if (newPosts.length === 100) {
+      page += 1
+      await getPosts(page);
+    }
   };
 
   try {
-      await getPosts();
+    await getPosts();
   } catch (error) {
     if (!posts.length) {
       return `Could not load posts from Wordpress (${url}) ${error}`;
@@ -414,7 +206,7 @@ exports.syncCategoriesWithWordPress = functions.pubsub.schedule('0 * * * *').tim
       description: category.description,
       name: category.name,
       taxonomy: category.taxonomy
-    }, { 
+    }, {
       merge: true
     });
   }
@@ -460,4 +252,31 @@ exports.syncPagesWithWP = functions.pubsub.schedule('0 1 * * *').timeZone("Europ
 
   await batch.commit();
   return "Successfully updated pages";
+});
+
+exports.getNewPlayers = functions.pubsub.schedule('0 14 * * 1').timeZone("Europe/Berlin").onRun(async (): Promise<any> => {
+  const supabase = createClient(functions.config().supabase.url, functions.config().supabase.key);
+  const response = await supabase
+    .from<any>('player')
+    .select('*')
+    .gt("joined", dayjs().subtract(2, "month").toISOString())
+    .order("instrument")
+    .order("lastName");
+
+  if (response.data && response.data.length) {
+    let message: string = `Neue Spieler:\n`;
+
+    for (const player of response.data) {
+      message += `${player.firstName} ${player.lastName}\n`;
+    }
+
+    const bot = new telegraf.Telegram(functions.config().bot.token, {});
+    bot.sendMessage(
+      functions.config().bot.group,
+      message
+    );
+    return "successfully send";
+  } else {
+    return "no data";
+  }
 });
