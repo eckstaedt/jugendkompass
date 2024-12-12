256ea3c711a0e8c91c3fd1e2e5a687edda87b24d
Matthias Eckstaedt <matthias.eckstaedt@sap.com>
2024-12-12 12:37:07 +0100

    update angular to 15

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -23,9 +23,8 @@ const routes: Routes = [
 @NgModule({
   imports: [
     RouterModule.forRoot(routes, {
-      preloadingStrategy: PreloadAllModules,
-      relativeLinkResolution: 'legacy',
-    }),
+    preloadingStrategy: PreloadAllModules
+}),
   ],
   exports: [RouterModule],
 })
